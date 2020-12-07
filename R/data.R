37aa1d6d2b21574e6581d93b5f5e78690f31b8f3
Felipe Dzul <felipe.dzul.m@gmail.com>
2020-12-07 06:42:24 -0600

    this commit add the package

---
+++ R/data.R
@@ -0,0 +1,199 @@
+#' Polygons of blocks from México of the National Electoral Institute 2020
+#'
+#' A spatial dataset containing of polygon data of blocks from México
+#'
+#' @format A sf object with 2278337 rows and 19 variables:
+#' \describe{
+#'   \item{gid}{the ids.}
+#'   \item{id}{the ids.}
+#'   \item{entidad}{is the numeric key of state.}
+#'   \item{distrito}{is the numeric key of federal district.}
+#'   \item{municipio}{is the numeric key to the Municipality}
+#'   \item{seccion}{is the numeric key to the Sectors}
+#'   \item{localidad}{is numeric the key of locality}
+#'   \item{manzana}{is the numeric key of blocks}
+#'   \item{geometry}{simple feature.}
+#'   ...
+#' }
+#' @source \url{https://pautas.ine.mx/transparencia/mapas/}
+#' @details This datasets were publiched in october 2020.
+"blocks_ine20_mx_a"
+
+#' Polygons of blocks from México of the National Electoral Institute 2020
+#'
+#' A spatial dataset containing of polygon data of blocks from México
+#'
+#' @format A sf object with 2278337 rows and 19 variables:
+#' \describe{
+#'   \item{gid}{the ids.}
+#'   \item{id}{the ids.}
+#'   \item{entidad}{is the numeric key of state.}
+#'   \item{distrito}{is the numeric key of federal district.}
+#'   \item{municipio}{is the numeric key to the Municipality}
+#'   \item{seccion}{is the numeric key to the Sectors}
+#'   \item{localidad}{is numeric the key of locality}
+#'   \item{manzana}{is the numeric key of blocks}
+#'   \item{geometry}{simple feature.}
+#'   ...
+#' }
+#' @source \url{https://pautas.ine.mx/transparencia/mapas/}
+#' @details This datasets were publiched in october 2020.
+"blocks_ine20_mx_b"
+
+#' Polygons of blocks from México of the National Electoral Institute 2020
+#'
+#' A spatial dataset containing of polygon data of blocks from México
+#'
+#' @format A sf object with 2278337 rows and 19 variables:
+#' \describe{
+#'   \item{gid}{the ids.}
+#'   \item{id}{the ids.}
+#'   \item{entidad}{is the numeric key of state.}
+#'   \item{distrito}{is the numeric key of federal district.}
+#'   \item{municipio}{is the numeric key to the Municipality}
+#'   \item{seccion}{is the numeric key to the Sectors}
+#'   \item{localidad}{is numeric the key of locality}
+#'   \item{manzana}{is the numeric key of blocks}
+#'   \item{geometry}{simple feature.}
+#'   ...
+#' }
+#' @source \url{https://pautas.ine.mx/transparencia/mapas/}
+#' @details This datasets were publiched in october 2020.
+"blocks_ine20_mx_c"
+
+#' Polygons of blocks from México of the National Electoral Institute 2020
+#'
+#' A spatial dataset containing of polygon data of blocks from México
+#'
+#' @format A sf object with 2278337 rows and 19 variables:
+#' \describe{
+#'   \item{gid}{the ids.}
+#'   \item{id}{the ids.}
+#'   \item{entidad}{is the numeric key of state.}
+#'   \item{distrito}{is the numeric key of federal district.}
+#'   \item{municipio}{is the numeric key to the Municipality}
+#'   \item{seccion}{is the numeric key to the Sectors}
+#'   \item{localidad}{is numeric the key of locality}
+#'   \item{manzana}{is the numeric key of blocks}
+#'   \item{geometry}{simple feature.}
+#'   ...
+#' }
+#' @source \url{https://pautas.ine.mx/transparencia/mapas/}
+#' @details This datasets were publiched in october 2020.
+"blocks_ine20_mx_d"
+
+#' Polygons of blocks from México of the National Electoral Institute 2020
+#'
+#' A spatial dataset containing of polygon data of blocks from México
+#'
+#' @format A sf object with 2278337 rows and 19 variables:
+#' \describe{
+#'   \item{gid}{the ids.}
+#'   \item{id}{the ids.}
+#'   \item{entidad}{is the numeric key of state.}
+#'   \item{distrito}{is the numeric key of federal district.}
+#'   \item{municipio}{is the numeric key to the Municipality}
+#'   \item{seccion}{is the numeric key to the Sectors}
+#'   \item{localidad}{is numeric the key of locality}
+#'   \item{manzana}{is the numeric key of blocks}
+#'   \item{geometry}{simple feature.}
+#'   ...
+#' }
+#' @source \url{https://pautas.ine.mx/transparencia/mapas/}
+#' @details This datasets were publiched in october 2020.
+"blocks_ine20_mx_e"
+
+#' Polygons of AGEBs from México of the National Institute of Geography and Statistics 2019
+#'
+#' A spatial dataset containing of polygon data of AGEBs from México
+#'
+#' @format A sf object with 80202 rows and 10 variables:
+#' \describe{
+#'   \item{OBJECTID}{the ids.}
+#'   \item{CVEGEO}{the string key. The key included the CVE_ENT, CVE_MUN, CVE_LOC and CVE_AGEB}
+#'   \item{CVE_ENT}{is the string key for each state.}
+#'   \item{CVE_MUN}{is the string key to the Municipality.}
+#'   \item{CVE_LOC}{is string the key of locality.}
+#'   \item{CVE_AGEB}{is the string key of AGEB.}
+#'   \item{Ambito}{is a binary variable with two values, Urbana and Rural. The dataset is inly AGEB Urban}
+#'   \item{Shape_Leng}{is the length of polygon}
+#'   \item{geometry}{simple feature.}
+#'   ...
+#' }
+#' @source \url{https://www.inegi.org.mx/temas/mg/#Descargas}
+#' @details AGEB (Areas GeoEstadisticas Basicas) is stand for basic geostatistical areas. This datasets were published in september 2019.
+"AGEB_inegi_2019_a"
+
+#' Polygons of AGEBs from México of the National Institute of Geography and Statistics 2019
+#'
+#' A spatial dataset containing of polygon data of AGEBs from México
+#'
+#' @format A sf object with 80202 rows and 10 variables:
+#' \describe{
+#'   \item{OBJECTID}{the ids.}
+#'   \item{CVEGEO}{the string key. The key included the CVE_ENT, CVE_MUN, CVE_LOC and CVE_AGEB}
+#'   \item{CVE_ENT}{is the string key for each state.}
+#'   \item{CVE_MUN}{is the string key to the Municipality.}
+#'   \item{CVE_LOC}{is string the key of locality.}
+#'   \item{CVE_AGEB}{is the string key of AGEB.}
+#'   \item{Ambito}{is a binary variable with two values, Urbana and Rural. The dataset is inly AGEB Urban}
+#'   \item{Shape_Leng}{is the length of polygon}
+#'   \item{geometry}{simple feature.}
+#'   ...
+#' }
+#' @source \url{https://www.inegi.org.mx/temas/mg/#Descargas}
+#' @details AGEB (Areas GeoEstadisticas Basicas) is stand for basic geostatistical areas. This datasets were published in september 2019.
+"AGEB_inegi_2019_b"
+
+
+#' Polygons of AGEEs from México of the National Institute of Geography and Statistics 2019
+#
+#' A spatial dataset containing of polygon data of AGEE from México
+#'
+#' @format A sf object with 32 rows and 4 variables:
+#' \describe{
+#'   \item{CVEGEO}{the numeric key. The key included the CVE_ENT.}
+#'   \item{CVE_ENT}{is the numeric key for each state.}
+#'   \item{NOMGEO}{is a string value. Its the name of state.}
+#'   \item{geometry}{simple feature.}
+#'   ...
+#' }
+#' @source \url{https://www.inegi.org.mx/temas/mg/#Descargas}
+#' @details AGEE (Areas GeoEstadisticas Estatales) is stand for State Geostatistical Areas. This datasets were published in september 2019.
+"AGEE_inegi19_mx"
+
+#' Polygons of AGEMs from México of the National Institute of Geography and Statistics 2019
+#
+#' A spatial dataset containing of polygon data of AGEM from México
+#'
+#' @format A sf object with 2465 rows and 5 variables:
+#' \describe{
+#'   \item{CVEGEO}{the numeric key. The key included the CVE_ENT and the CVE_MUN.}
+#'   \item{CVE_ENT}{is the numeric key for each state.}
+#'   \item{CVE_MUN}{is the string key to the Municipality.}
+#'   \item{NOMGEO}{is a string value. Its the name of municipality.}
+#'   \item{geometry}{simple feature.}
+#'   ...
+#' }
+#' @source \url{https://www.inegi.org.mx/temas/mg/#Descargas}
+#' @details AGEM (Areas GeoEstadisticas Municipales) is stand for Municipality Geostatistical Areas. This datasets were published in september 2019.
+"AGEM_inegi19_mx"
+
+#' Polygons of localities from México of the National Institute of Geography and Statistics 2019
+#
+#' A spatial dataset containing of polygon data of localities from México
+#'
+#' @format A sf object with 49981 rows and 7 variables:
+#' \describe{
+#'   \item{CVEGEO}{the numeric key. The key included the CVE_ENT and the CVE_MUN.}
+#'   \item{CVE_ENT}{is the numeric key for each state.}
+#'   \item{CVE_MUN}{is the string key to the Municipality.}
+#'   \item{CVE_LOC}{is the string key of locality.}
+#'   \item{NOMGEO}{is a string value. Its the name of locality.}
+#'   \item{Ambito}{is a binary variable with two values, Urbana and Rural.}
+#'   \item{geometry}{simple feature.}
+#'   ...
+#' }
+#' @source \url{https://www.inegi.org.mx/temas/mg/#Descargas}
+#' @details This datasets were published in september 2019.
+"loc_inegi19_mx"
