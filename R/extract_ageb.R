c04686ce283344403a14ee040d059586e96c5953
Felipe Dzul <felipe.dzul.m@gmail.com>
2021-01-05 14:57:02 -0600

    This commit add the extract_ageb function

---
+++ R/extract_ageb.R
@@ -0,0 +1,32 @@
+#' extract_ageb
+#'
+#' This function extract the AGEB of the locality.
+#'
+#' @param locality locality target.
+#' @param cve_geo is a string of the state id.
+#'
+#' @return a list with two sf objects: a locality limit and the agebs of locality target
+#' @export
+#' @author Felipe Antonio Dzul Manzanilla \email{felipe.dzul.m@gmail.com}.
+#'
+#' @examples 1+1
+extract_ageb <- function(locality, cve_geo){
+
+    x <- rgeomex::loc_inegi19_mx %>%
+        dplyr::filter(NOMGEO %in%
+                          c(similiars::find_most_similar_string(c(locality),
+                                                                unique(NOMGEO))) &
+                          CVE_ENT %in% c(cve_geo))
+    y <- rbind(rgeomex::AGEB_inegi_2019_a,
+               rgeomex::AGEB_inegi_2019_b)
+
+    xy <- y[x,]
+
+    multi_return <- function() {
+        my_list <- list("locality" = x,
+                        "ageb" = xy)
+        return(my_list)
+    }
+    multi_return()
+
+}
